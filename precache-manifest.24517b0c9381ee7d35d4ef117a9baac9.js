168fcccc02be14ac13fdf6d803eee193f6347edc
olgab <olgabresheva7@gmail.com>
2020-08-22 02:09:45 -0700

    Updates

---
+++ precache-manifest.24517b0c9381ee7d35d4ef117a9baac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c71981dfc7efc93c8703b108bf7a04",
+    "url": "/kanbanredux21/index.html"
+  },
+  {
+    "revision": "57717413884a861be1cc",
+    "url": "/kanbanredux21/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "219a730f056b2ee28aba",
+    "url": "/kanbanredux21/static/css/main.fc772dfe.chunk.css"
+  },
+  {
+    "revision": "57717413884a861be1cc",
+    "url": "/kanbanredux21/static/js/2.414c06c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kanbanredux21/static/js/2.414c06c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219a730f056b2ee28aba",
+    "url": "/kanbanredux21/static/js/main.c76ba082.chunk.js"
+  },
+  {
+    "revision": "e18d9ca2883f6a622e1d",
+    "url": "/kanbanredux21/static/js/runtime-main.4a1a8716.js"
+  }
+]);
